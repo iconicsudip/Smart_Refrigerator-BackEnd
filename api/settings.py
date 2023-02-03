6a0c68589033526186da36ea88afbef8f888b411
iconicsudip <das366966@gmail.com>
2023-02-03 15:48:08 +0530

    vercel settings

--- api/settings.py
+++ api/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-lm5kap)lz8zpv+5y^81%kyhw=yvhys)0jqf-b=sx@xvvt60!!f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+
 
 
 # Application definition
